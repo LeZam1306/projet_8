69b09f232cec9d53f3d3bee2f1eb0caa23f02f0b
Adam Zamouri <adamzamouricontact@gmail.com>
2025-02-07 13:52:15 +0100

    Optimization of site accessibility and SEO performance

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,29 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const section = document.getElementById("gallery")
+
+    const observation = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting){
+            
+            $(document).ready(function() {
+
+                $('.gallery').mauGallery({
+                    columns: {
+                        xs: 1,
+                        sm: 2,
+                        md: 3,
+                        lg: 3,
+                        xl: 3
+                    },
+                    lightBox: true,
+                    lightboxId: 'myAwesomeLightbox',
+                    showTags: true,
+                    tagsPosition: 'top'
+                });
+            
+            });
+            
+            observation.disconnect();
+        }
+    })
+    observation.observe(section);
+}); 
