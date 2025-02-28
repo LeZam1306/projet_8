d65468b5fc1526553d3ee4f835c554624e95e393
Adam Zamouri <adamzamouricontact@gmail.com>
2025-02-28 14:48:30 +0100

    Improvement of the following points: - Minification of js and css. - Correction of contrast errors on Wave - Meta tag that is less than 150 characters.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,29 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const section = document.getElementById("gallery")
-
-    const observation = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting){
-            
-            $(document).ready(function() {
-
-                $('.gallery').mauGallery({
-                    columns: {
-                        xs: 1,
-                        sm: 2,
-                        md: 3,
-                        lg: 3,
-                        xl: 3
-                    },
-                    lightBox: true,
-                    lightboxId: 'myAwesomeLightbox',
-                    showTags: true,
-                    tagsPosition: 'top'
-                });
-            
-            });
-            
-            observation.disconnect();
-        }
-    })
-    observation.observe(section);
-}); 
+document.addEventListener("DOMContentLoaded",()=>{const section=document.getElementById("gallery")
+    const observation=new IntersectionObserver(entries=>{if(entries[0].isIntersecting){$(document).ready(function(){$('.gallery').mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:'myAwesomeLightbox',showTags:!0,tagsPosition:'top'})});observation.disconnect()}})
+    observation.observe(section)})
